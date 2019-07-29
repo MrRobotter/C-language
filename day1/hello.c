df283c2d527fc63b18bd76d96e8527bbc77559d8
zhangjunyang <2816886869@qq.com>
2019-07-29 11:02:17 +0800

    函数库

--- day1/hello.c
+++ day1/hello.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main(){
-	printf("hello,world,this is Joinyon`s first C software....");
+	printf("hello,world,this is Joinyon`s first C software....\n");
 	return 0;
 
 // 这是备注
